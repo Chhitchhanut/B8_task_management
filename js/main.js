8c262b091ea3c2c516f64d5c04aec1558b0b4f81
SREYKEO.KEUN <SREYKEO.KEUN@PNC.LOCAL>
2025-12-24 11:02:28 +0700

    feature/search-card

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// function toggleDropdown() {
+//     const menu = document.getElementById('dropdownMenu');
+//     menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
+// }
+
+// function filterPractice(category) {
+//     const items = document.querySelectorAll('#practiceList .list-item');
+//     items.forEach(item => {
+//         if (category === 'All' || item.dataset.category === category) {
+//             item.style.display = 'list-item';
+//         } else {
+//             item.style.display = 'none';
+//         }
+//     });
+//     document.getElementById('dropdownMenu').style.display = 'none';
+// }
